cd5e221a9b631015c1602816106a7fafc75778eb
Prajita <prajitacst@gmail.com>
2018-12-05 08:57:24 +0530

    port update:

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './dist',
-        port: 3000
+        port: process.env.PORT || 3000
     },
     devtool: "source-map",
     module: {
