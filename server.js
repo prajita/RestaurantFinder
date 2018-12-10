c60f72d42f6c5ff662aa45d72e519415a09fab75
itsmeprajita <prajitacst@gmail.com>
2018-12-10 11:16:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path =  require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname,'dist')));
+console.log('Inside server js ', path.join(__dirname,'dist'));
 app.get('/*',function(req,res){
 res.sendfile(path.join(__dirname,'dist','index.html'))
 });
