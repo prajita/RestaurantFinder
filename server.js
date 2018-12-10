9ced13542e659bfad002f979f8524678b16db63b
itsmeprajita <prajitacst@gmail.com>
2018-12-10 10:10:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path =  require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,'dist')));
 app.get('/*',function(req,res){
-res.sendfile(path.join(__dirname,'build','index.html'))
+res.sendfile(path.join(__dirname,'dist','index.html'))
 });
-app.listen(process.env.PORT,()=>{console.log(""app is up!!)})
+app.listen(3000,()=>{console.log("app is up!!")})
