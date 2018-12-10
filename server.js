4524a5484763199673a8077b010f249b92e2d4a5
Prajita <prajitacst@gmail.com>
2018-12-10 19:48:28 +0530

    update

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static(path.join(__dirname,'dist')));
 console.log('Inside server js ', path.join(__dirname,'dist'));
 app.get('/*',function(req,res){
-res.sendfile(path.join(__dirname,'dist','index.html'))
+res.sendFile(path.join(__dirname,'dist','index.html'))
 });
 app.listen(3000,()=>{console.log("app is up!!")})
