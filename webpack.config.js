3e474b3b7c0256e0232bb4030c23df286a978bb0
Prajita <prajitacst@gmail.com>
2018-12-10 21:28:31 +0530

    port 5000 check

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './dist',
-        port: 3000,
+        port: 5000,
         historyApiFallback: true,
     },
     plugins: [
