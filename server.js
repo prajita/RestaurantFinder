98eaba5a58d9330baa5ae3413532ad929e224b9a
itsmeprajita <prajitacst@gmail.com>
2018-12-11 08:45:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path =  require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname,'dist')));
+app.use(express());
+app.use(express().json());
+app.use("/", express.static(`${__dirname}/dist`));
+
+/*app.use(express.static(path.join(__dirname,'dist')));
 console.log('Inside server js ', path.join(__dirname,'dist'));
 app.get('/*',function(req,res){
 res.sendFile(path.join(__dirname,'dist','index.html'))
-});
+});*/
 var port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0',()=>{console.log("app is up!!")})
